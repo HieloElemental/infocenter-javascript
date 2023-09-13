441615a7f0f23cdd089aab6bd24bc9ffc862bf0a
HieloElemental <hieloelemental@gmail.com>
2023-09-12 21:02:37 -0500

    Added greeting

---
+++ main.js
@@ -0,0 +1,6 @@
+const rootElement = document.getElementById("root");
+
+let myName = "HieloElemental";
+let message = `Hi there! I'm ${myName}`;
+rootElement.innerText = message;
+console.log(message);
