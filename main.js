86891ed47348b18c1106e44638348a34334e951d
HieloElemental <hieloelemental@gmail.com>
2023-09-18 19:54:59 -0500

    Add "not found" case

--- main.js
+++ main.js
@@ -1,6 +1,28 @@
 const rootElement = document.getElementById("root");
+let rootInnerText = "";
 
-let myName = "HieloElemental";
+const myName = "HieloElemental";
 let message = `Hi there! I'm ${myName}`;
+rootInnerText += message;
+
+let arrayNumbers = [2, 5, 7, 11, 13, 17];
+const searchOnArray = (arr, element) => {
+  let pos = 0;
+  while (pos < arr.length) {
+    if (arr[pos] == element) {
+      return pos;
+    }
+    pos++;
+  }
+  return null;
+};
+let searchedNumber = 13;
+let foundedPosition = searchOnArray(arrayNumbers, searchedNumber);
+if (foundedPosition) {
+  rootInnerText += `\nThe founded item position is: ${foundedPosition}`;
+} else {
+  rootInnerText += `\nThe element isn't in the array`;
+}
+
 rootElement.innerText = message;
 console.log(message);
