92ef2bdd323678afc05338c19be850790bc57e8b
HieloElemental <hieloelemental@gmail.com>
2023-10-27 20:58:44 -0500

    changed API url

--- onlineShop/src/util/productsProvider.js
+++ onlineShop/src/util/productsProvider.js
@@ -1,7 +1,7 @@
 const getProducts = async () => {
   let localStorageProducts = await localStorage.getItem("products");
   if (!localStorageProducts) {
-    let products = await fetch("https://fakestoreapi.com/products");
+    let products = await fetch("fakestore.alejandroguti53.repl.co/products");
     let data = await products.json();
     localStorage.setItem("products", JSON.stringify(data));
     return data;
