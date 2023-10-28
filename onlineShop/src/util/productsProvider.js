66f624e97d3169c9cad87a9aca8b29a65832ac78
Hielo Elemental <136840526+HieloElemental@users.noreply.github.com>
2023-10-27 21:07:45 -0500

    Update productsProvider.js

--- onlineShop/src/util/productsProvider.js
+++ onlineShop/src/util/productsProvider.js
@@ -1,7 +1,7 @@
 const getProducts = async () => {
   let localStorageProducts = await localStorage.getItem("products");
   if (!localStorageProducts) {
-    let products = await fetch("fakestore.alejandroguti53.repl.co/products");
+    let products = await fetch("https://fakestore.alejandroguti53.repl.co/products");
     let data = await products.json();
     localStorage.setItem("products", JSON.stringify(data));
     return data;
